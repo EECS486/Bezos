d0fac45ffcf4fefd746d7e4ad76d426204586cf4
pmascheroni <petermas@umich.edu>
2018-04-18 22:32:26 -0400

    precalc done

--- reviewdataNB.py
+++ reviewdataNB.py
@@ -75,7 +75,7 @@ class Review:
 
 
 # Read in review data if there is no pre calculated file
-def readInReviewData(fileName, preCalcFileName, minVoteThreshold, minReviewLength):
+def readInReviewData(fileName, minVoteThreshold, minReviewLength):
     reviewsList = []
     dataStore = []
 
